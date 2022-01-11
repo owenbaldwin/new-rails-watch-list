f6154082c870863339453fb0d3e49c673a66d067
Owen Baldwin <owenabaldwin@gmail.com>
2022-01-11 17:40:54 +0000

    controllers and list index done

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,7 +1,7 @@
 class List < ApplicationRecord
-  has_many :bookmarks, dependant: :destroy
+  has_many :bookmarks, dependent: :destroy
   has_many :movies, through: :bookmarks
-  # has_many :reviews, dependant: :destroy
+  # has_many :reviews, dependent: :destroy
 
   validates :name, presence: true, uniqueness: true
 
