a33c49aed9ad669ac324b3dfe9d6562e29d3b76c
Owen Baldwin <owenabaldwin@gmail.com>
2022-01-11 17:56:39 +0000

    basic list new done

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,4 +5,26 @@ class ListsController < ApplicationController
     @lists = List.all
   end
 
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_list
+    @list = List.find(params[:id])
+  end
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
 end
