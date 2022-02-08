5dfc415ecc773c4ebc2968a8787a3f0621f8db42
Owen Baldwin <owenabaldwin@gmail.com>
2022-02-08 13:14:22 +0000

    sudo code added

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -11,3 +11,11 @@ end
 # parse IMDb like in cookbook challenge
 # store the selected movie in csv file
 # list page for movies to list all movies to make sure they are stored somehwere.
+
+# in new movie view
+# search for title in search bar
+# store the search in a variable
+# interpolate the variable in the url
+# in new bookmark view
+# display on screen top 5 results for search
+# create a add to list button to create a bookmark
